9e5e67cadc3acf43e7109b43ffeb5f0a2e4736fc
TimurSennikov <88229024+TimurSennikov@users.noreply.github.com>
2023-12-31 17:52:12 +0000

    Add files via upload

--- module.c
+++ module.c
@@ -1,56 +1,69 @@
-#include <linux/init.h>
-#include <linux/module.h>
-#include <linux/gpio.h>
-#include <linux/gpio/consumer.h>
+#include <linux/init.h>                                                                        
+#include <linux/module.h>                                                                      
+#include <linux/gpio.h>                                                                        
+#include <linux/gpio/consumer.h>                                                               
 #include <linux/printk.h>
+#include <linux/fs.h>
+#include <linux/uaccess.h>
+#include <linux/fcntl.h>
 
-#define LIMIT 70000
-#define GPIOFANPIN 4
-#define DELAY 1
+#define MAX_BUF 64
+#define LIMIT 70000                                                                            
+#define GPIOFANPIN 4                                                                           
+#define DELAY 1                                                                                
 
-char* zone = "thermal_zone0";
+char* zone = "thermal_zone0";                                                                  
+static struct gpio_desc *fan;                                                                  
+int voltage;
+float temperature = 0;
 
-static struct gpio_desc *fan;
-
-int temperature;
-
-static int getcputemp()
+static int getcputemp(void)                                                                    
 {
-FILE* fp;
-char path[MAX_BUF];
-snprintf(path, sizeof(path), "/sys/class/thermal/%s/temp", zone);
-fp = fopen(path, "r");
-if (fp == NULL)
-{
-perror("error opening file");
-return 1;
-	}
-int temperature = (fp, "%d", &temperature);
+    struct file *fp;
+    char path[MAX_BUF];
+    int voltage;
+    int temperature;
+    ssize_t ret;
 
-fscanf(fp "%d", &temperature);
+    snprintf(path, sizeof(path), "/sys/class/thermal/%s/temp", zone);
+    
+    fp = filp_open(path, O_RDONLY, 0);
+    if (IS_ERR(fp))
+    {
+        pr_err("error opening file: %ld\n", PTR_ERR(fp));
+        return 1;
+    }
 
-fclose(fp);
+    ret = kernel_read(fp, &temperature, sizeof(int), &fp->f_pos);
+
+    filp_close(fp, NULL);
+
+    return ret;
 }
 
-static int fandigitset()
+static int fandigitset(void)                                                                   
 {
-if(temperature >= LIMIT)
-{
-set_gpio_value(GPIOFANPIN, 1);
-voltage = 1;
-	}
-else{
-if (voltage == 1){
-set_gpio_value(GPIOFANPIN, 0);
-voltage = 0;
-		}
-	}
-}
-static int __init main(void)
-{
-    int ret = 0;
+    if (temperature >= LIMIT)
+    {
+        gpiod_set_value(fan, 1);
+        voltage = 1;
+    }
+    else
+    {
+        if (voltage == 1)
+        {
+            gpiod_set_value(fan, 0);
+            voltage = 0;
+        }
+    }
 
-    fan = gpio_to_desc(4);
+    return 0;
+}
+static int main_init(void)                                                                   
+{
+    int ret = 0;                                                                               
+
+    fan = gpio_to_desc(GPIOFANPIN);    // Use GPIOFANPIN instead of hardcoding 4
     if (!fan) {
         pr_err("Failed to get GPIO descriptor\n");
         return -ENODEV;
@@ -63,13 +76,14 @@ static int __init main(void)
     return ret;
 }
 
-static void __exit intrpt_exit(void)
+static void main_exit(void)                                                           
 {
     gpiod_put(fan);
 }
 
-module_init(main);
-module_exit(intrpt_exit);
+module_init(main_init);
+module_exit(main_exit);
 
 MODULE_LICENSE("GPL");
 MODULE_DESCRIPTION("GPIO fan Control");
+
